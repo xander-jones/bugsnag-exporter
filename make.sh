ab20dbec4dbc38c78b049f30407a37187112ecd8
Xander Jones <xander@bugsnag.com>
2021-08-24 22:22:09 +0100

    improved build script

--- make.sh
+++ make.sh
@@ -3,16 +3,8 @@
 # https://github.com/golang/go/blob/master/src/go/build/syslist.go
 
 os_archs=(
-    aix/ppc64
     darwin/386
     darwin/amd64
-    dragonfly/amd64
-    freebsd/386
-    freebsd/amd64
-    freebsd/arm
-    freebsd/arm64
-    illumos/amd64
-    js/wasm
     linux/386
     linux/amd64
     linux/arm
@@ -25,53 +17,47 @@ os_archs=(
     linux/mips64le
     linux/riscv64
     linux/s390x
-    netbsd/386
-    netbsd/amd64
-    netbsd/arm
-    netbsd/arm64
-    openbsd/386
-    openbsd/amd64
-    openbsd/arm
-    openbsd/arm64
-    plan9/386
-    plan9/amd64
-    plan9/arm
-    solaris/amd64
     windows/386
     windows/amd64
     windows/arm
 )
 
-os_archs_32=()
-os_archs_64=()
+build_success=()
+build_failures=()
 
+printf "Building bugsnag-to-csv binaries for the chosen platforms & architectures in ./build\r\n\r\n"
+# clear out the ./build directory to begin with
+# and then recreate it so we have a fresh output.
 if [ -d "build" ]; then
     rm -rf build
 fi
 mkdir "build"
 
+idx=1
 for os_arch in "${os_archs[@]}"
 do
+    printf "\33[2K\rBuilding binary %s of ${#os_archs[@]} ($os_arch)" $idx
     goos=${os_arch%/*}
     goarch=${os_arch#*/}
-    GOOS=${goos} GOARCH=${goarch} go build -o ./build/bugsnag-to-csv_$goos_$goarch main.go
+    GOOS=${goos} GOARCH=${goarch} go build -o "./build/$os_arch/bugsnag-to-csv" main.go &> /dev/null
     if [ $? -eq 0 ]
     then
-        os_archs_64+=(${os_arch})
+        build_success+=(${os_arch})
     else
-        os_archs_32+=(${os_arch})
+        build_failures+=(${os_arch})
     fi
+    ((idx=idx+1))
 done
 
-echo "32-bit:"
-for os_arch in "${os_archs_32[@]}"
+printf "\33[2K\rSuccesfuly built for:\r\n"
+for os_arch in "${build_success[@]}"
 do
     printf "\t%s\n" "${os_arch}"
 done
 echo
 
-echo "64-bit:"
-for os_arch in "${os_archs_64[@]}"
+printf "Failed to build for:\r\n"
+for os_arch in "${build_failures[@]}"
 do
     printf "\t%s\n" "${os_arch}"
 done
