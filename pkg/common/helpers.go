5a60838d5ce47ed28d964d9225770e039e57c9ff
Xander Jones <xander@bugsnag.com>
2021-09-09 15:04:11 +0100

    add the helper functions

--- pkg/common/helpers.go
+++ pkg/common/helpers.go
@@ -1,11 +1,33 @@
 package common
 
-func PrintHeader() {
-	print("##################################################")
-	print("#                                                #")
-	print("#              Bugsnag-to-CSV, v%s            #", PackageVersion)
-	print("#                Xander Jones, 2021              #")
-	print("# https://github.com/xander-jones/bugsnag-to-csv #")
-	print("#                                                #")
-	print("##################################################")
+import (
+	"fmt"
+	"os"
+)
+
+func ExitWithError(errorCode int, errorMessage string) {
+	Print("[ERROR] %s\n", errorMessage)
+	os.Exit(errorCode)
+}
+
+func Print(message string, args ...interface{}) {
+	fmt.Printf("[bugsnag-to-csv] "+message+"\r\n", args...)
+}
+
+func PrintVerbose(verbose *bool, message string, args ...interface{}) {
+	if *verbose {
+		Print(message, args)
+	}
+}
+
+func PrintHeader(verbose *bool) {
+	if *verbose {
+		Print("##################################################")
+		Print("#                                                #")
+		Print("#              Bugsnag-to-CSV, v%s            #", PackageVersion)
+		Print("#                Xander Jones, 2021              #")
+		Print("# https://github.com/xander-jones/bugsnag-to-csv #")
+		Print("#                                                #")
+		Print("##################################################")
+	}
 }
