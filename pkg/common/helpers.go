bb458517c0bcb4f3ef36a6c76dede22ec17ac94e
Xander Jones <xander@bugsnag.com>
2021-09-16 19:36:08 +0100

    comments are good

--- pkg/common/helpers.go
+++ pkg/common/helpers.go
@@ -5,23 +5,61 @@ import (
 	"os"
 )
 
+/*
+	Variable to control output verbosity.
+	Defaults to false
+*/
 var Verbose bool = false
 
-func ExitWithError(errorCode int, errorMessage string) {
+/*
+	Exits the application with the error code given after
+	printing an error message string to the console.
+*/
+func ExitWithString(errorCode int, errorMessage string) {
 	Print("[ERROR] %s\n", errorMessage)
 	os.Exit(errorCode)
 }
 
+/*
+	Exits the application with the error code given after
+	printing an error message from an error object to console.
+*/
+func ExitWithError(errorCode int, err error) {
+	Print("[ERROR] %s\n", err.Error())
+	os.Exit(errorCode)
+}
+
+/*
+	Exits the application with the error code given after
+	printing a string and message from an error object to console.
+*/
+func ExitWithErrorAndString(errorCode int, err error, errorMessage string) {
+	Print("[ERROR] %s\n", errorMessage)
+	Print("[ERROR] %s\n", err.Error())
+	os.Exit(errorCode)
+}
+
+/*
+	Prints a message to the console, can take in arguments
+	after the initial message so you can treat this as a Sprintf()
+*/
 func Print(message string, args ...interface{}) {
 	fmt.Printf("[bugsnag-to-csv] "+message+"\r\n", args...)
 }
 
+/*
+	Prints a message to the console, in the same way as Print()
+	but ONLY if `Verbose` is set to true
+*/
 func PrintVerbose(message string, args ...interface{}) {
 	if Verbose {
-		Print(message, args)
+		fmt.Printf("[b2c----VERBOSE] "+message+"\r\n", args...)
 	}
 }
 
+/*
+	Prints a header to the console, including the application version
+*/
 func PrintHeader() {
 	if Verbose {
 		Print("##################################################")
