19b9d24804122d87ff8fb55f103b4231fbd53273
Xander Jones <xander@bugsnag.com>
2021-11-15 13:32:45 +0100

    write errors to file

--- pkg/daa/error.go
+++ pkg/daa/error.go
@@ -8,11 +8,11 @@ import (
 )
 
 func GetError(projectId string, errorId string, filters string) map[string]interface{} {
-	// Docs: https://bugsnagapiv2.docs.apiary.io/#reference/errors/errors/view-an-error
-	//   GET https://api.bugsnag.com/projects/project_id/errors/error_id
+	// Docs https://bugsnagapiv2.docs.apiary.io/#reference/errors/errors/view-an-error
+	//  GET https://api.bugsnag.com/projects/project_id/errors/error_id
 	var url string = addQueryParams("https://api.bugsnag.com/projects/"+projectId+"/errors/"+errorId, filters)
 	common.PrintVerbose("Getting error from API: " + url)
-	var handle *os.File = writers.CreateNewOutputFile(projectId, "error-"+errorId)
+	var handle *os.File = writers.CreateNewOutputFile(projectId, "errorId-"+errorId)
 	var err map[string]interface{} = BugsnagGetObject(url)
 	writers.WriteObjectToFile(handle, err)
 	writers.CloseOutputFile(handle)
@@ -20,11 +20,11 @@ func GetError(projectId string, errorId string, filters string) map[string]inter
 }
 
 func GetErrorEvents(projectId string, errorId string, filters string) []map[string]interface{} {
-	// Docs: https://bugsnagapiv2.docs.apiary.io/#reference/errors/events/list-the-events-on-an-error
-	//   GET https://api.bugsnag.com/projects/project_id/errors/error_id/events
+	// Docs https://bugsnagapiv2.docs.apiary.io/#reference/errors/events/list-the-events-on-an-error
+	//  GET https://api.bugsnag.com/projects/project_id/errors/error_id/events
 	var url string = addQueryParams("https://api.bugsnag.com/projects/"+projectId+"/errors/"+errorId+"/events", filters)
 	common.PrintVerbose("Getting events from API: " + url)
-	var handle *os.File = writers.CreateNewOutputFile(projectId, "events-of-"+errorId)
+	var handle *os.File = writers.CreateNewOutputFile(projectId, "events-of-errorId-"+errorId)
 	var events []map[string]interface{} = BugsnagGetArray(url)
 	writers.WriteArrayToFile(handle, events)
 	writers.CloseOutputFile(handle)
--- pkg/daa/project.go
+++ pkg/daa/project.go
@@ -1,23 +1,32 @@
 package daa
 
 import (
+	"os"
+
 	"github.com/xander-jones/bugsnag-exporter/pkg/common"
+	"github.com/xander-jones/bugsnag-exporter/pkg/writers"
 )
 
 func GetProjectErrors(projectId string, filters string) []map[string]interface{} {
-	// Docs: https://bugsnagapiv2.docs.apiary.io/#reference/errors/errors/list-the-errors-on-a-project
-	//   GET https://api.bugsnag.com/projects/project_id/errors
+	// Docs https://bugsnagapiv2.docs.apiary.io/#reference/errors/errors/list-the-errors-on-a-project
+	//  GET https://api.bugsnag.com/projects/project_id/errors
 	var url string = addQueryParams("https://api.bugsnag.com/projects/"+projectId+"/errors", filters)
 	common.PrintVerbose("Getting errors from API: " + url)
+	var handle *os.File = writers.CreateNewOutputFile(projectId, "errors-of-projectId-"+projectId)
 	var errs []map[string]interface{} = BugsnagGetArray(url)
+	writers.WriteArrayToFile(handle, errs)
+	writers.CloseOutputFile(handle)
 	return errs
 }
 
 func GetProjectEvents(projectId string, filters string) []map[string]interface{} {
-	// Docs: https://bugsnagapiv2.docs.apiary.io/#reference/errors/events/list-the-events-on-a-project
-	//   GET https://api.bugsnag.com/projects/project_id/events
+	// Docs https://bugsnagapiv2.docs.apiary.io/#reference/errors/events/list-the-events-on-a-project
+	//  GET https://api.bugsnag.com/projects/project_id/events
 	var url string = addQueryParams("https://api.bugsnag.com/projects/"+projectId+"/events", filters)
 	common.PrintVerbose("Getting events from API: " + url)
+	var handle *os.File = writers.CreateNewOutputFile(projectId, "events-of-projectId-"+projectId)
 	var events []map[string]interface{} = BugsnagGetArray(url)
+	writers.WriteArrayToFile(handle, events)
+	writers.CloseOutputFile(handle)
 	return events
 }
